36f5934b70bed28c72b9a2d5e4972bb96f8e7b18
Ninos Ego <me@ninosego.de>
2016-06-07 02:45:51 +0200

    Enhancement: Usage of ABSPATH then function check (default way) (#5)
    * Enhancement: Usage of ABSPATH then function check (default way)

--- tgm-example-plugin.php
+++ tgm-example-plugin.php
@@ -35,11 +35,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-// Avoid direct calls to this file.
-if ( ! function_exists( 'add_action' ) ) {
-	header( 'Status: 403 Forbidden' );
-	header( 'HTTP/1.1 403 Forbidden' );
-	exit();
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
 if ( ! function_exists( 'tgm_php_mysql_versions' ) ) {
